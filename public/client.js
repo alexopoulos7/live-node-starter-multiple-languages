9da97565c1a3e905e0ffff66e1a382bae8e836ff
Angelos Alexopoulos <angelosalexopoulos@cloud.upwork.com>
2024-08-13 11:38:16 +0300

    Add 5 different languages in server and client, ui

--- public/client.js
+++ public/client.js
@@ -1,3 +1,23 @@
+const TRANSCRIPTION_LANGUAGES = [
+  {'Chinese (Mandarin, Simplified)':{'model-suffix':'zh', 'port':3003}},
+  {'English': {'model-suffix':'en', 'port':3004}},
+  {'French': {'model-suffix':'fr', 'port':3005}},
+  {'German': {'model-suffix':'de', 'port':3006}},
+  {'Multilingual (Spanish + English)': {'model-suffix':'multi', 'port':3007}}
+]
+
+const maxReconnectInterval = 30000; // Maximum 30 seconds between reconnects
+const languageSelected = document.getElementById('listeningLanguage');
+
+
+let sharedData = {
+  sockets: [],
+  current_socket: null,
+  listening_language: null,
+  seconds: 0,
+  microphone: null
+}
+
 const captions = window.document.getElementById("captions");
 
 async function getMicrophone() {
@@ -10,7 +30,7 @@ async function getMicrophone() {
   }
 }
 
-async function openMicrophone(microphone, socket) {
+async function openMicrophone(microphone) {
   return new Promise((resolve) => {
     microphone.onstart = () => {
       console.log("client: microphone opened");
@@ -25,8 +45,8 @@ async function openMicrophone(microphone, socket) {
 
     microphone.ondataavailable = (event) => {
       console.log("client: microphone data received");
-      if (event.data.size > 0 && socket.readyState === WebSocket.OPEN) {
-        socket.send(event.data);
+      if (event.data.size > 0 && sharedData.current_socket.readyState === WebSocket.OPEN) {
+        sharedData.current_socket.send(event.data);
       }
     };
 
@@ -38,7 +58,28 @@ async function closeMicrophone(microphone) {
   microphone.stop();
 }
 
-async function start(socket) {
+function populateLanguageDropdowns() {
+  const listeningDropdown = document.getElementById('listeningLanguage');
+  TRANSCRIPTION_LANGUAGES.forEach(lang => {
+      const langName = Object.keys(lang)[0];
+      const option = new Option(langName, langName);
+      listeningDropdown.add(option.cloneNode(true));
+  
+      if (langName === 'English') {
+        listeningDropdown.value = langName;
+    }
+  });
+}
+
+
+function getLanguageFromTranscriptionLanguages(language) {
+  let languageObj = TRANSCRIPTION_LANGUAGES.find(obj => Object.keys(obj)[0].toLowerCase().startsWith(language.toLowerCase()));
+  if (!languageObj) return null;
+  let key = Object.keys(languageObj)[0];
+  return languageObj[key];
+}
+
+async function start() {
   const listenButton = document.querySelector("#record");
   let microphone;
 
@@ -48,7 +89,7 @@ async function start(socket) {
     if (!microphone) {
       try {
         microphone = await getMicrophone();
-        await openMicrophone(microphone, socket);
+        await openMicrophone(microphone);
       } catch (error) {
         console.error("error opening microphone:", error);
       }
@@ -59,33 +100,88 @@ async function start(socket) {
   });
 }
 
-window.addEventListener("load", () => {
-  const socket = new WebSocket("ws://localhost:3000");
 
-  socket.addEventListener("open", async () => {
-    console.log("client: connected to server");
-    await start(socket);
-  });
-
-  socket.addEventListener("message", (event) => {
-    if (event.data === "") {
-      return;
-    }
-    
-    let data;
-    try {
-      data = JSON.parse(event.data);
-    } catch (e) {
-      console.error("Failed to parse JSON:", e);
-      return;
-    }
+const connectWebSocket = () => { 
+  let reconnectAttempts = 0;
+  let reconnectInterval = 1000; // Initial reconnect interval
+   
   
-    if (data && data.channel && data.channel.alternatives[0].transcript !== "") {
-      captions.innerHTML = `<span>${data.channel.alternatives[0].transcript}</span>`;
-    }
-  });
+  TRANSCRIPTION_LANGUAGES.forEach(languageObj => {
+    const language_name = Object.keys(languageObj)[0];
+    const language = getLanguageFromTranscriptionLanguages(language_name);
+    
+    const current_socket = new WebSocket(`ws://localhost:${language.port}/`)
+    sharedData.sockets.push(current_socket);
+    console.log(`ws://localhost:${language.port}/`)
 
-  socket.addEventListener("close", () => {
-    console.log("client: disconnected from server");
+    current_socket.addEventListener("open", async (event) => {
+      console.log("client: connected to server");
+      console.log("client: waiting to open microphone");
+      reconnectAttempts = 0; // Reset reconnect attempts on successful connection
+    });
+
+    current_socket.addEventListener("message", (event) => {
+      const data = JSON.parse(event.data);
+      if (data && data.channel && data.channel.alternatives[0].transcript !== "") {
+        captions.innerHTML = `<span>${data.channel.alternatives[0].transcript}</span>`;
+      } 
+      
+    });  
+
+    current_socket.addEventListener("close", (e) => {
+      console.log(e)
+      console.log("client: disconnected from server");
+     
+        if (reconnectAttempts < maxReconnectAttempts) {
+          reconnectAttempts++;
+          reconnectInterval = Math.min(reconnectInterval * 2, maxReconnectInterval); // Exponential backoff
+          setTimeout(() => {
+            console.log(`client: attempting to reconnect (attempt ${reconnectAttempts})...`);
+            //connectWebSocket();
+          }, reconnectInterval);
+        } else {
+          console.error("client: maximum reconnect attempts reached");
+        } 
+    });
+  
+    current_socket.addEventListener("error", (error) => {
+      console.error('WebSocket Error: ', error);
+      showError(error_message, "Connection to the server failed. Please try again later.");
+      current_socket.close(); // Close the socket to trigger reconnect logic
+    }); 
+  
   });
+  
+  sharedData.current_socket = sharedData.sockets[0];
+  sharedData.listening_language = document.getElementById('listeningLanguage').value || 'English';
+  let currentLanguageIndex = TRANSCRIPTION_LANGUAGES.findIndex(languageObj => Object.keys(languageObj)[0] === sharedData.listening_language);
+  sharedData.current_socket = sharedData.sockets[currentLanguageIndex];
+   
+}
+
+async function updateListeningLanguage() {
+  sharedData.listening_language = languageSelected.value;
+  const language_index = TRANSCRIPTION_LANGUAGES.findIndex(lang => Object.keys(lang)[0] === sharedData.listening_language);
+  console.log('Updated listening language:', sharedData.listening_language);
+  
+  // Stop current microphone and socket
+  if (sharedData.microphone) {
+    await closeMicrophone(sharedData.microphone);
+  }
+  
+  // Set the new socket
+  sharedData.current_socket = sharedData.sockets[language_index];
+  
+  // Restart the microphone with the new socket
+  sharedData.microphone = await getMicrophone();
+  await openMicrophone(sharedData.microphone);
+}
+
+window.addEventListener("load", () => {
+  populateLanguageDropdowns();
+  languageSelected.addEventListener('change', updateListeningLanguage);
+  connectWebSocket();
+  start()
+  
+  
 });
--- server.js
+++ server.js
@@ -3,97 +3,156 @@ const http = require("http");
 const WebSocket = require("ws");
 const { createClient, LiveTranscriptionEvents } = require("@deepgram/sdk");
 const dotenv = require("dotenv");
+
 dotenv.config();
-
-const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-const deepgramClient = createClient(process.env.DEEPGRAM_API_KEY);
+const TRANSCRIPTION_LANGUAGES = [
+  {'Chinese (Mandarin, Simplified)':{'model-suffix':'zh', 'port':3003}},
+  {'English': {'model-suffix':'en', 'port':3004}},
+  {'French': {'model-suffix':'fr', 'port':3005}},
+  {'German': {'model-suffix':'de', 'port':3006}},
+  {'Multilingual (Spanish + English)': {'model-suffix':'multi', 'port':3007}}
+]
 let keepAlive;
 
-const setupDeepgram = (ws) => {
-  const deepgram = deepgramClient.listen.live({
+const createStableClient = () => {
+  try {
+    return createClient(process.env.DEEPGRAM_API_KEY);
+  } catch (e) {
+    console.error('Error fetching user usage details:', e);
+    return createClient(process.env.DEEPGRAM_API_KEY2);
+  }
+};
+
+const setupDeepgram = (ws, language, modelSuffix) => {
+  console.log(`Deepgram: Setting up for language ${language} and modelSuffix ${modelSuffix}`);
+  const deepgram = createStableClient().listen.live({
+    language: modelSuffix,
+    punctuate: true,
     smart_format: true,
     model: "nova-2",
+    diarize: true,
   });
 
-  if (keepAlive) clearInterval(keepAlive);
-  keepAlive = setInterval(() => {
-    console.log("deepgram: keepalive");
-    deepgram.keepAlive();
-  }, 10 * 1000);
 
-  deepgram.addListener(LiveTranscriptionEvents.Open, async () => {
-    console.log("deepgram: connected");
+  const startKeepAlive = () => {
+    if (keepAlive) clearInterval(keepAlive);
+    keepAlive = setInterval(() => {
+      if (deepgram.getReadyState() === 1) {
+        console.log("Deepgram: Keeping connection alive");
+        deepgram.keepAlive();
+      }
+    }, 5000); // Increased interval to 5 seconds to reduce unnecessary calls
+  };
+
+  deepgram.addListener(LiveTranscriptionEvents.Open, () => {
+    console.log("Deepgram: Connected");
+    startKeepAlive();
 
     deepgram.addListener(LiveTranscriptionEvents.Transcript, (data) => {
-      console.log("deepgram: transcript received");
-      console.log("ws: transcript sent to client");
-      ws.send(JSON.stringify(data));
+      console.log("Deepgram: Transcript received");
+      if (ws.readyState === WebSocket.OPEN) {
+        ws.send(JSON.stringify(data));
+      }
     });
 
-    deepgram.addListener(LiveTranscriptionEvents.Close, async () => {
-      console.log("deepgram: disconnected");
+    deepgram.addListener(LiveTranscriptionEvents.Close, () => {
+      console.log("Deepgram: Disconnected");
+      clearInterval(keepAlive);
+    });
+
+    deepgram.addListener(LiveTranscriptionEvents.Error, (error) => {
+      console.error("Deepgram: Error received", error);
       clearInterval(keepAlive);
       deepgram.finish();
     });
 
-    deepgram.addListener(LiveTranscriptionEvents.Error, async (error) => {
-      console.log("deepgram: error received");
-      console.error(error);
-    });
-
-    deepgram.addListener(LiveTranscriptionEvents.Warning, async (warning) => {
-      console.log("deepgram: warning received");
-      console.warn(warning);
+    deepgram.addListener(LiveTranscriptionEvents.Warning, (warning) => {
+      console.warn("Deepgram: Warning received", warning);
     });
 
     deepgram.addListener(LiveTranscriptionEvents.Metadata, (data) => {
-      console.log("deepgram: metadata received");
-      console.log("ws: metadata sent to client");
-      ws.send(JSON.stringify({ metadata: data }));
+      console.log(`Deepgram: Metadata received for ${language} ${JSON.stringify(data)}`);
+      if (ws.readyState === WebSocket.OPEN) {
+        ws.send(JSON.stringify({ metadata: data }));
+      }
     });
-  });
+  }); 
 
   return deepgram;
 };
 
-wss.on("connection", (ws) => {
-  console.log("ws: client connected");
-  let deepgram = setupDeepgram(ws);
+const servers = TRANSCRIPTION_LANGUAGES.map(languageObj => {
+  const language = Object.keys(languageObj)[0];
+  const { 'model-suffix': modelSuffix, port } = languageObj[language];
 
-  ws.on("message", (message) => {
-    console.log("ws: client data received");
+  const app = express();
+  const server = http.createServer(app);
+  const wss = new WebSocket.Server({ server });
 
-    if (deepgram.getReadyState() === 1 /* OPEN */) {
-      console.log("ws: data sent to deepgram");
-      deepgram.send(message);
-    } else if (deepgram.getReadyState() >= 2 /* 2 = CLOSING, 3 = CLOSED */) {
-      console.log("ws: data couldn't be sent to deepgram");
-      console.log("ws: retrying connection to deepgram");
-      /* Attempt to reopen the Deepgram connection */
-      deepgram.finish();
-      deepgram.removeAllListeners();
-      deepgram = setupDeepgram(ws);
-    } else {
-      console.log("ws: data couldn't be sent to deepgram");
-    }
+  wss.on("connection", (ws) => {
+    console.log(`Socket: Client connected on port ${port}`);
+    let deepgram = setupDeepgram(ws, language.toLowerCase(), modelSuffix);
+    let reconnectAttempts = 0;
+    const maxReconnectAttempts = 50000;
+  
+    const reconnectToDeepgram = () => {
+      if (reconnectAttempts < maxReconnectAttempts) {
+        console.log(`Socket: Retrying connection to Deepgram ${language.toLowerCase()}(Attempt ${reconnectAttempts + 1})`);
+        deepgram = setupDeepgram(ws, language.toLowerCase(), modelSuffix);
+        reconnectAttempts++;
+        return deepgram.getReadyState() === 1;
+      }
+      console.log(`Socket: Max reconnection attempts reached ${language.toLowerCase()}`);
+      return false;
+    };
+  
+    ws.on("message", (message) => {
+      console.log("Deepgram state is", deepgram.getReadyState());
+      console.log(`Socket: Client data received ${language.toLowerCase()}`);
+      if (deepgram.getReadyState() === 1) {
+        console.log(`Socket: Data sent to Deepgram ${language.toLowerCase()}`);
+        deepgram.send(message);
+        reconnectAttempts = 0; // Reset reconnect attempts on successful send
+      } else if (reconnectToDeepgram()) {
+        deepgram.send(message);
+        console.log(`Socket: Data sent to Deepgram after reconnection ${language.toLowerCase()}`);
+      } else {
+        console.log(`Socket: Data couldn't be sent to Deepgram, ${language.toLowerCase()}`);
+        console.log(deepgram)
+      }
+    }); 
+
+    ws.on("error", (error) => {
+      console.error("Socket: Error", error);
+      ws.close();
+    });
+
+    ws.on("close", () => {
+      console.log(`Socket: Client disconnected for language ${language}`);
+      clearInterval(keepAlive);
+      try {
+        if (deepgram) {
+          deepgram.finish();
+          deepgram.removeAllListeners();
+        }
+      } catch (error) {
+        console.error(" ${language} Error during cleanup:", error);
+      }
+    });
+
   });
 
-  ws.on("close", () => {
-    console.log("ws: client disconnected");
-    deepgram.finish();
-    deepgram.removeAllListeners();
-    deepgram = null;
+  app.use(express.static("public/"));
+
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
   });
+
+  return { server, port, language };
 });
 
-app.use(express.static("public/"));
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-server.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+servers.forEach(({ server, port, language }) => {
+  server.listen(port, () => {
+    console.log(`Server for ${language} is listening on port ${port}`);
+  });
 });
